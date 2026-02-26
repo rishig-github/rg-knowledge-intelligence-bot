edf377c79c89419c8a4cba667cf7f39fd6a35a64
rishi <rishi.csc@gmail.com>
2026-02-26 09:29:48 -0500

    Storage Service added

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_KEY")
+    AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+    AWS_REGION = os.getenv("AWS_REGION")
+    VECTOR_DB_PATH = "vector_db"
+
